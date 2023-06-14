ab02911c6cadc595acab83e917cd27bf14b8c662
wyang2001 <91300316+wyang2001@users.noreply.github.com>
2023-06-14 14:05:38 +0800

    Update config.ts
    gpt-3.5 turbo 增加16k

--- app/store/config.ts
+++ app/store/config.ts
@@ -81,6 +81,10 @@ export const ALL_MODELS = [
     available: true,
   },
   {
+    name: "gpt-3.5-turbo-16k",
+    available: true,
+  },
+  {
     name: "gpt-3.5-turbo-0301",
     available: true,
   },
